aac564406b614c8a8e4b42c6b9b7088b9b373376
deepaksp24 <96986851+deepaksp24@users.noreply.github.com>
2024-07-10 22:49:59 +0530

    till part 3rd folder  6th part

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("jan", views.index)
+    # path("jan", views.january),
+    # path("feb", views.february),
+    # path("mar", views.march),
+    path("<month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("this works")
+# def january(request):
+#     return HttpResponse("full motivation month")
+
+
+# def february(request):
+#     return HttpResponse("walk atleast 20minutes a day")
+
+
+# def march(request):
+#     return HttpResponse("focus on yourself")
+
+
+def monthly_challenge(request, month):
+    if month == 'january':
+        challenge_test = "full motivation month"
+    elif month == 'february':
+        challenge_test = "walk atleast 20minutes a day"
+    elif month == 'march':
+        challenge_test = "do somthing produvtive"
+    else:
+        return HttpResponseNotFound("this month is not supported")
+    return HttpResponse(challenge_test)
