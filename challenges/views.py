5b7b563cb62a2d7d9389cebeef8f042631efb875
deepaksp24 <96986851+deepaksp24@users.noreply.github.com>
2024-07-14 00:15:58 +0530

    4th folder 9th completed

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -17,14 +18,19 @@ from django.urls import reverse
 #     return HttpResponse("focus on yourself")
 
 def index(request):
-    list_items = ""
+    # list_items = ""
     months = list(mothly_challenges.keys())
-    for month in months:
-        captitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href= \"{month}\">{captitalized_month}</a></li>"
-    response_data = f"<ol>{list_items}</ol>"
-    return HttpResponse(response_data)
+    # for month in months:
+    #     captitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href= \"{month}\">{captitalized_month}</a></li>"
+    # response_data = f"<ol>{list_items}</ol>"
+    # return HttpResponse(response_data)
+
+    return render(request, "challenges\index.html", {
+        "months": months
+
+    })
 
 
 mothly_challenges = {
@@ -45,7 +51,11 @@ def mothly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = mothly_challenges[month]
-        respose_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(respose_data)
+        return render(request, "challenges/challenge.html", {
+            "month_name": month,
+            "text": challenge_text,
+        })
+        # respose_data = render_to_string("challenges/challenge.html")
+        # return HttpResponse(respose_data)
     except:
         return HttpResponse("<h1>this month is not supported</h1>")
